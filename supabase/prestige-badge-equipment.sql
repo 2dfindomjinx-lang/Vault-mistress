b929c5a8879f85b43f1b7ffbb3cdbbcdbb132634
2dfindomjinx-lang <2dfindomjinx@gmail.com>
2026-08-06 02:39:49 +0300

    1.13.1

--- supabase/prestige-badge-equipment.sql
+++
@@ -1,5 +0,0 @@
-alter table public.user_prestige_badges
-  add column if not exists equipped boolean not null default false;
-
-alter table public.user_prestige_badges
-  alter column equipped set default false;
